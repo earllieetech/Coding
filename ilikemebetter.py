75a7c96d53f4a59c3199a844c9bdf571fe61919f
earlliee_03 <catigumgemeearl@gmail.com>
2026-06-23 18:41:06 +0800

    Add files via upload

---
+++ ilikemebetter.py
@@ -0,0 +1,40 @@
+import time
+from threading import Thread, Lock
+import sys
+
+lock = Lock()
+
+def animate_text(text, delay=0.1):
+    with lock:
+        for char in text:
+            sys.stdout.write(char)
+            sys.stdout.flush()
+            time.sleep(delay)
+        print()
+
+def sing_lyric(lyric, delay, speed):
+    time.sleep(delay)
+    animate_text(lyric, speed)
+
+def sing_song():
+    lyrics = [
+        ("\n""I knew from the first time", 0.09),
+        ("I'd stay for a long time 'cause", 0.09),
+        ("I like me better when", 0.09),
+        ("I like me better when I'm with you", 0.08),
+        
+    ]
+    delays = [0.3, 3.0, 6.6, 9.6]
+    
+    threads = []
+    for i in range(len(lyrics)):
+        lyric, speed = lyrics[i]
+        t = Thread(target=sing_lyric, args=(lyric, delays[i], speed))
+        threads.append(t)
+        t.start()
+    
+    for thread in threads:
+        thread.join()
+
+if __name__ == "__main__":
+    sing_song()
